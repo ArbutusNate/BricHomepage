7b8a68bc5053dab3783ea7ea36f2996be570e6c0
ArbutusNate <arbutusnate@gmail.com>
2018-06-01 14:46:35 -0700

    adding media queries, removing JQ on smaller screens for now

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
 $(window).ready(function() {
   $('.hidden').hide();
   $('.btt').hide();
+  $(window).resize();
 })
 
-//Functions
-//Fix header and blurb
-  // const theFixer = function(pos){
-  //   $(window).scroll(function(e){
-
-  //   })
-  // }
+//Large Size fancy JS
+if ($(window).width() > 426){
   $.fn.followFrom = function (pos) {
       var $this = this,
           $window = $(window);
-
-          console.log(`Triggering at ${pos}`)
 
       $window.scroll(function (e) {
           if ($window.scrollTop() > pos) {
@@ -22,7 +16,7 @@ $(window).ready(function() {
               $('#header-nav').addClass('sticky');
             //Add Sticky to #blurb
               let blurbWidth = $('#blurb').width()
-              console.log(blurbWidth);
+              // console.log(blurbWidth);
               $('#blurb').addClass('sticky').width(blurbWidth);
             //Adjust padding on background
               $('.background').css(
@@ -30,10 +24,6 @@ $(window).ready(function() {
               )
             //Move Logo
               $('#logo').addClass('sticky');
-              // $('#title-bar').css({
-              //   "float": "left",
-              //   "z-index": "6"
-              // })
           } else {
             //Remove Stickies
               $('#header-nav').removeClass('sticky');
@@ -44,12 +34,17 @@ $(window).ready(function() {
               })
             //Reset Logo
               $('#logo').removeClass('sticky');
-              // $('#title-bar').css(
-              //   "float", "none"
-              // )
           }
       });
   };
+  //Listeners
+    $('#header-nav').followFrom(58);
+} else {
+  console.log('small screen');
+}
+
+
+//Functions
 
 //Managing Sample Tabs
   const sampleTabs = function(event){
@@ -66,10 +61,6 @@ $(window).ready(function() {
   const scrollToTop = function(){
     window.scrollTo(0,0);
   }
-
-//Listeners
-  $('#header-nav').followFrom(58);
-  // $('#blurb').followFrom(55);
 
 //On Clicks
 //Tab Buttons

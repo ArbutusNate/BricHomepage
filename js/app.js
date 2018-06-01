5ae58f42cd1fd18ccf797bffe3abc0ac425b37a8
ArbutusNate <arbutusnate@gmail.com>
2018-06-01 12:59:57 -0700

    navbar is sticking to top of screen well, need to figure out the content jumping around and add functionality to blurb box

--- js/app.js
+++ js/app.js
@@ -5,14 +5,51 @@ $(window).ready(function() {
 
 //Functions
 //Fix header and blurb
-  const theFixer = function(event){
-    
-  }
+  // const theFixer = function(pos){
+  //   $(window).scroll(function(e){
+
+  //   })
+  // }
+  $.fn.followFrom = function (pos) {
+      var $this = this,
+          $window = $(window);
+
+          console.log(`Triggering at ${pos}`)
+
+      $window.scroll(function (e) {
+          if ($window.scrollTop() > pos) {
+              // $this.css({
+              //     position: 'initial',
+              //     top: pos
+              // });
+              $this.addClass('sticky');
+              // $('.background').css(
+              //     "padding-top", "100px"
+              //   )
+          } else {
+              $this.removeClass('sticky');
+              // $('.background').css(
+              //     "padding-top", "initial"
+              //   );
+
+              // $this.css({
+              //     position: 'fixed',
+              //     top: 0
+              //     // z-index: 10
+              // });
+              // $('.background').css({
+              //     padding: 50
+              // })
+          }
+      });
+  };
 
 //Managing Sample Tabs
   const sampleTabs = function(event){
+    $('.tab-btn').removeClass('tab-btn-active');
     $('.hidden').hide();
-    let chosen = $(this).attr("data-btn");
+    $(this).addClass('tab-btn-active');
+    let chosen = $(this).attr('data-btn');
     let location = $(this).offset();
     $('#' + chosen).show();
     window.scrollTo(location);
@@ -23,6 +60,8 @@ $(window).ready(function() {
     window.scrollTo(0,0);
   }
 
+//Listeners
+  $('#header-nav').followFrom(55);
 
 //On Clicks
 //Tab Buttons

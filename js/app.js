4672135afc43c83bd16b70a88a460d727331e459
ArbutusNate <arbutusnate@gmail.com>
2018-06-08 11:53:40 -0700

    tweaked show more buttons

--- js/app.js
+++ js/app.js
@@ -1,56 +1,55 @@
 const seeMore = " See More Information ";
 const seeLess = " See Less ";
+var btnWidth = 0;
 
 
 $(window).ready(function() {
   $('.hidden').hide();
   $('.btt').hide();
-  // $(window).resize();
 })
 
 //Large Size fancy JS
-if ($(window).width() > 426){
-  $.fn.followFrom = function (pos) {
-      var $this = this,
-          $window = $(window);
+  if ($(window).width() > 426){
+    $.fn.followFrom = function (pos) {
+        var $this = this,
+            $window = $(window);
 
-      $window.scroll(function (e) {
-          if ($window.scrollTop() > pos) {
-            //Add Sticky to #header-nav
-              $('#header-nav').addClass('sticky');
-            //Add Sticky to #blurb
-              let blurbWidth = $('#blurb').width()
-              console.log(blurbWidth);
-              $('#blurb').addClass('sticky').width(blurbWidth);
-            //Adjust padding on background
-              $('.background').css(
-                "padding-top", "68px"
-              )
-            //Move Logo
-              $('#logo').addClass('sticky');
-          } else {
-            //Remove Stickies
-              $('#header-nav').removeClass('sticky');
-              $('#blurb').removeClass('sticky');
-            //Reset Padding
-              $('.background').css({
-                "padding": "20px 150px"
-              })
-            //Reset Logo
-              $('#logo').removeClass('sticky');
-          }
-      });
-  };
-  //Listeners
-    $('#header-nav').followFrom(58);
-} else {
-  console.log('small screen');
-}
+        $window.scroll(function (e) {
+            if ($window.scrollTop() > pos) {
+              //Add Sticky to #header-nav
+                $('#header-nav').addClass('sticky');
+              //Add Sticky to #blurb
+                let blurbWidth = $('#blurb').width()
+                $('#blurb').addClass('sticky').width(blurbWidth);
+              //Adjust padding on background
+                $('.background').css(
+                  "padding-top", "68px"
+                )
+              //Move Logo
+                $('#logo').addClass('sticky');
+            } else {
+              //Remove Stickies
+                $('#header-nav').removeClass('sticky');
+                $('#blurb').removeClass('sticky');
+              //Reset Padding
+                $('.background').css({
+                  "padding": "20px 150px"
+                })
+              //Reset Logo
+                $('#logo').removeClass('sticky');
+            }
+        });
+    };
+    //Listeners
+      $('#header-nav').followFrom(58);
+  } else {
+    console.log('small screen');
+  }
 
 
 //Functions
 
-//Managing Sample Tabs
+//Sample Tabs
   const sampleTabs = function(event){
     $('.tab-btn').removeClass('tab-btn-active');
     $('.hidden').hide();
@@ -61,33 +60,37 @@ if ($(window).width() > 426){
       var location = $(this).offset();
     } else {
       var location = $('.memoir-container').offset();
-      console.log(location);
       location.top = location.top - 63;
     }
     $('#blurb').hide();
     window.scrollTo(location);
     $('.btt').show();
   }
-//Scroll to Top Function
+
+//Scroll to Top Button
   const scrollToTop = function(){
     window.scrollTo(0,0);
     $('#blurb').show();
   }
-//Fancy Text
+
+//"Show More/Less" Buttons
   const showMore = function(event){
-    console.log($(this).text());
     let currentText = $(this).text();
+    let currentWidth = $(this).outerWidth();
+    // var btnWidth = currentWidth;
+
     if (currentText == seeMore){
-      return $(this).text(seeLess);
+      $(this).text(seeLess).css("width", currentWidth);
     };
+
     if (currentText == seeLess){
-      return $(this).text(seeMore);
+      $(this).text(seeMore);
     };
   }
 
 //On Clicks
-//Tab Buttons
-  $('.tab-btn').on('click', sampleTabs);
-//Back To Top Button
-  $('.btt').on('click', scrollToTop);
-  $('.smi-btn').on('click', showMore);
+  //Tab Buttons
+    $('.tab-btn').on('click', sampleTabs);
+  //Back To Top Button
+    $('.btt').on('click', scrollToTop);
+    $('.smi-btn').on('click', showMore);
